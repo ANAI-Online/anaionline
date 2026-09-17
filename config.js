339972334ae723955bf37a1661d19266c2c9faa2
jonathanheredia88 <jonathanheredia88@gmail.com>
2026-09-17 10:28:51 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+window.ANAI_CONFIG = {
+  supabaseUrl: "https://xortjmsciegafcbbepdn.supabase.co",
+  supabaseAnonKey: "sb_publishable_GpheNjAQMPsmZmv15jF6vw_HFS2V7MD",
+  institucion: "ANAI Online",
+  anioLectivo: "2027-2028"
+};
