06c2df94ed0c18572cbda28c0f1e30e860dbf34e
jonathanheredia88 <jonathanheredia88@gmail.com>
2026-09-18 09:29:10 -0500

    Delete backend directory

--- backend/src/errors.ts
+++
@@ -1,9 +0,0 @@
-export class HttpError extends Error {
-  constructor(public status: number, message: string) {
-    super(message);
-  }
-}
-export const notFound = (what = 'El recurso') => new HttpError(404, `${what} no existe o no tienes acceso.`);
-export const forbidden = (msg = 'No tienes permiso para esta acción.') => new HttpError(403, msg);
-export const badRequest = (msg: string) => new HttpError(400, msg);
-export const conflict = (msg: string) => new HttpError(409, msg);
